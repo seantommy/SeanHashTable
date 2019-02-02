c5052a6cd6b1415036e15c7b165637e92af89cc6
Sean Edgar <seantommye@gmail.com>
2019-02-01 19:35:58 -0600

    Add project files.

---
+++ SeanHashTable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeanHashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SeanHashTable table = new SeanHashTable();
+
+            table.Add("pants");
+            table.Add("dog");
+            table.Add("Greg");
+            table.Add("Daniel Avidan");
+            table.Add("Greg Smith");
+            table.Add("");
+
+            bool nameFound = table.Search("Greg");
+            nameFound = table.Search("weggie");
+            nameFound = table.Search("Daniel Avidan");
+
+            table.Delete("Greg");
+            table.Delete("wow");
+
+            nameFound = table.Search("Greg");
+
+            string name = "human ";
+            for(int x = 0; x < 5000; x++)
+            {
+                table.Add(name);
+                name = "human " + x.ToString();
+            }
+
+            nameFound = table.Search("pants");
+        }
+    }
+}
---
+++ SeanHashTable/SeanHashTable.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeanHashTable
+{
+    class SeanHashTable
+    {
+        private string[] table = new string[1000];
+        private string[] resizingTable;
+        private bool resizing = false;
+        private int resizeIndex = 0;
+        public int Count { get; private set; }
+        public SeanHashTable() { Count = 0; }
+
+        public void Add(string newValue)
+        {
+            int index;
+            
+            if (resizing || Count >= table.Length * .75) //If we're resizing, we move three values over from the original table to the new table.
+            {
+                if (!resizing)
+                {
+                    resizing = true;
+                    resizingTable = new string[table.Length * 2];
+                }
+
+                for (int x = 0; x < 3; x++)
+                {
+                    if (table[resizeIndex] != null)
+                    {
+                        index = GetHashIndex(table[resizeIndex]);
+                        PlaceInTable(table[resizeIndex], index, resizingTable);
+                    }
+                    resizeIndex++;
+                    if (resizeIndex >= table.Length)
+                    {
+                        resizing = false;
+                        resizeIndex = 0;
+                        table = null;
+                        table = resizingTable;
+                        break;
+                    }
+                }
+
+                index = GetHashIndex(newValue);
+                PlaceInTable(newValue, index, resizingTable);
+            }
+            else
+            {
+                index = GetHashIndex(newValue);
+                PlaceInTable(newValue, index, table);
+            }
+
+            Count++;
+        }
+
+        private int GetHashIndex(string value)
+        {
+            int index = -1;
+            char[] representation = value.ToCharArray();
+            foreach (char character in representation)
+            {
+                index += GetCharHashValue(character);
+            }
+
+            if (index == 0)
+            {
+                index = 1;
+            }
+            else if (index < 0)
+            {
+                index = Math.Abs(index);
+            }
+
+            if (resizing)
+            {
+                index = index % (resizingTable.Length - 1);
+            }
+            else
+            {
+                index = index % (table.Length - 1);
+            }
+
+            return index;
+        }
+
+        private int GetCharHashValue(char character)
+        {
+            int index;
+            switch (character)
+            {
+                case 'a':
+                    index = 1;
+                    break;
+                case 'b':
+                    index = 2;
+                    break;
+                case 'c':
+                    index = 3;
+                    break;
+                case 'd':
+                    index = 4;
+                    break;
+                case 'e':
+                    index = 5;
+                    break;
+                case 'f':
+                    index = 6;
+                    break;
+                case 'g':
+                    index = 7;
+                    break;
+                case 'h':
+                    index = 8;
+                    break;
+                case 'i':
+                    index = 9;
+                    break;
+                case 'j':
+                    index = 10;
+                    break;
+                case 'k':
+                    index = 11;
+                    break;
+                case 'l':
+                    index = 12;
+                    break;
+                case 'm':
+                    index = 13;
+                    break;
+                case 'n':
+                    index = 14;
+                    break;
+                case 'o':
+                    index = 15;
+                    break;
+                case 'p':
+                    index = 16;
+                    break;
+                case 'q':
+                    index = 17;
+                    break;
+                case 'r':
+                    index = 18;
+                    break;
+                case 's':
+                    index = 19;
+                    break;
+                case 't':
+                    index = 20;
+                    break;
+                case 'u':
+                    index = 21;
+                    break;
+                case 'v':
+                    index = 22;
+                    break;
+                case 'w':
+                    index = 23;
+                    break;
+                case 'x':
+                    index = 24;
+                    break;
+                case 'y':
+                    index = 25;
+                    break;
+                case 'z':
+                    index = 26;
+                    break;
+                case '1':
+                    index = 1;
+                    break;
+                case '2':
+                    index = 2;
+                    break;
+                case '3':
+                    index = 3;
+                    break;
+                case '4':
+                    index = 4;
+                    break;
+                case '5':
+                    index = 5;
+                    break;
+                case '6':
+                    index = 6;
+                    break;
+                case '7':
+                    index = 7;
+                    break;
+                case '8':
+                    index = 8;
+                    break;
+                case '9':
+                    index = 9;
+                    break;
+                case '0':
+                    index = 10;
+                    break;
+                default:
+                    index = 0;
+                    break;
+            }
+
+            return index;
+        }
+
+
+
+        private void PlaceInTable(string value, int index, string[] table)
+        {
+            if (table[index] == null)
+            {
+                table[index] = value;
+            }
+            else
+            {
+                bool placed = false;
+                int cycles = 1;
+                while (!placed)
+                {
+                    if (cycles < 20)
+                    {
+                        index = (index + index - 1) % (table.Length - 1);      
+                    }
+                    else
+                    {
+                        index = (index + 1) % (table.Length - 1);
+                    }
+
+                    if (table[index] == null)
+                    {
+                        table[index] = value;
+                        placed = true;
+                    }
+                    cycles++;
+                }
+            }
+        }
+
+        public void Delete(string value)
+        {
+            KeyValuePair<bool, int> valueProbed = ProbeForValue(value);
+            if (valueProbed.Key)
+            {
+                if (resizing)
+                {
+                    if (resizingTable[valueProbed.Value] == value)
+                    {
+                        resizingTable[valueProbed.Value] = null;
+                    }
+                    if (table[valueProbed.Value] == value)
+                    {
+                        table[valueProbed.Value] = null;
+                    }
+                }
+                else
+                {
+                    table[valueProbed.Value] = null;
+                }
+            }
+        }
+
+        public bool Search(string value)
+        {
+            KeyValuePair<bool, int> valueProbed = ProbeForValue(value);
+            return valueProbed.Key;
+        }
+
+        private KeyValuePair<bool, int> ProbeForValue(string value)
+        {
+            bool found = false;
+            int index = GetHashIndex(value);
+            int oldIndex = index % (table.Length - 1);
+            int cycles = 0;
+
+            while (!found)
+            {
+                if (resizing)
+                {
+                    if (table[index] == null && resizingTable[index] == null)
+                    {
+                        break;
+                    }
+                    else if (resizingTable[index] == value)
+                    {
+                        found = true;
+                    }
+                    else if (table[oldIndex] == value)
+                    {
+                        found = true;
+                        index = oldIndex;
+                    }
+                    else
+                    {
+                        if (cycles < 20)
+                        {
+                            index = (index + index - 1) % (resizingTable.Length - 1);
+                            oldIndex = (oldIndex + oldIndex - 1) % (table.Length - 1);
+                        }
+                        else
+                        {
+                            index = (index + 1) % (resizingTable.Length - 1);
+                            oldIndex = (oldIndex + 1) % (table.Length - 1);
+                        }
+                    }
+                }
+                else
+                {
+
+                    if (table[index] == null)
+                    {
+                        break;
+                    }
+                    else if (table[index] == value)
+                    {
+                        found = true;
+                    }
+                    else
+                    {
+                        if (cycles < 20)
+                        {
+                            index = (index + index - 1) % (table.Length - 1);
+                        }
+                        else
+                        {
+                            index = (index + 1) % (table.Length - 1);
+                        }
+                    }
+                }
+                cycles++;
+            }
+            KeyValuePair<bool, int> indexProbed = new KeyValuePair<bool, int>(found, index);
+            return indexProbed;
+        }
+    }
+}
